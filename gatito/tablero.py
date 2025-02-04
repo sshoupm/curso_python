e7adb073fa37b4d068863a747826419fee9f7164
sshoupm <sofiashoup@gmail.com>
2025-02-04 10:59:49 -0700

    Agregamos función juego() y checa_winner(), ya se puede jugar

--- gatito/tablero.py
+++ gatito/tablero.py
@@ -49,6 +49,29 @@ def juego(simbolos:dict):
         ['1','5','9'],
         ['3','5','7']
         ]
+    en_juego=True
+    ganador=""
+    movimientos=0
+    dibuja_tablero(simbolos)
+    while en_juego:
+        if movimientos <9:
+            usuario(simbolos)
+            dibuja_tablero(simbolos)
+            movimientos += 1
+            gana=checa_winner(simbolos,lista_combinaciones)
+            if gana is True:
+                en_juego=False
+                ganador="usuario/a"
+            ia(simbolos)
+            dibuja_tablero(simbolos)
+            gana=checa_winner(simbolos,lista_combinaciones)
+            if gana is True:
+                en_juego=False
+                ganador="computadora"
+            if movimientos >=9:
+                en_juego=False
+        else:
+            en_juego=False
 
 def checa_winner(simbolos:dict, combinaciones:list):
     '''Checa si hay un ganador'''
@@ -60,12 +83,12 @@ def checa_winner(simbolos:dict, combinaciones:list):
 if __name__=='__main__':
     numeros=[str(i) for i in range(1,10)]
     dsimbolos={x:x for x in numeros}
-    dibuja_tablero(dsimbolos)
+    juego(dsimbolos)
+    '''dibuja_tablero(dsimbolos)
     ia(dsimbolos)
     dibuja_tablero(dsimbolos)
     usuario(dsimbolos)
     dibuja_tablero(dsimbolos)
-    '''
     x=random.choice(numeros)
     numeros.remove(x)
     dsimbolos[x]='X'
