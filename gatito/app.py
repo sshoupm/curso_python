1ce327847b105e325cfdf8b902222415e61022d0
sshoupm <sofiashoup@gmail.com>
2025-02-07 13:09:32 -0700

    Agregamos funciones actualiza_score() y despliega_tablero() para continuar jugando y mostrar el puntaje de cada juego

--- gatito/app.py
+++ gatito/app.py
@@ -5,13 +5,23 @@ import tablero
 
 def main():
     '''Funcion principal'''
+    X= {"G":0, "P":0, "E":0}
+    O= {"G":0, "P":0, "E":0}
+    score={"X":X,"O":O}
     numeros=[str(i) for i in range(1,10)]
-    dsimbolos={x:x for x in numeros}
-    g = tablero.juego(dsimbolos)
-    if g is not None:
+    corriendo=True
+    while corriendo:
+        dsimbolos={x:x for x in numeros}
+        g = tablero.juego(dsimbolos)
+        tablero.actualiza_score(score,g)
+        tablero.despliega_tablero(score)
+        seguir=input('Quieres seguir jugando? (s/n):')
+        if seguir.lower=='n':
+            corriendo=False
+    '''if g is not None:
         print(f'El ganador es {g}')
     else:
-        print('Empate')
+        print('Empate')'''
 
 if __name__ == '__main__':
     main()
--- gatito/tablero.py
+++ gatito/tablero.py
@@ -82,6 +82,33 @@ def checa_winner(simbolos:dict, combinaciones:list):
             return simbolos[c[0]]
     return None 
 
+def actualiza_score(score:dict,ganador:str):
+    '''Actualiza el score'''
+    X=score["X"]
+    O=score["O"]
+    if ganador is not None:
+        print(f'El ganador es {ganador}')
+        if ganador=='X':
+            X["G"] += 1
+            O["P"] += 1
+        elif ganador=='O':
+            O["G"] +=1
+            X["P"] +=1
+        else:
+            X["E"] +=1
+            O["E"] +=1
+    else:
+        print('Empate')
+        X["E"] += 1
+        O["E"] += 1
+
+def despliega_tablero(score:dict):
+    '''Despliega el tablero de score'''
+    print(f'''
+    X | G: {score["X"]["G"]} | P: {score["X"]["P"]} | E: {score["X"]["E"]}
+    O | G: {score["O"]["G"]} | P: {score["O"]["P"]} | E: {score["O"]["E"]}
+    ''')
+
 if __name__=='__main__':
     numeros=[str(i) for i in range(1,10)]
     dsimbolos={x:x for x in numeros}
