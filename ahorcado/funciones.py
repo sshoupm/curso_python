4b9a3939ed12b234bff6ee8261c1233ed3869132
sshoupm <sofiashoup@gmail.com>
2025-02-10 10:59:25 -0700

    Nuevo proyecto del juego ahorcado, se crean las plantillas.  y el archivo de funciones.py para cargarlas.

---
+++ ahorcado/funciones.py
@@ -0,0 +1,16 @@
+'''
+Funciones auxiliares del juego del ahorcado
+'''
+
+def carga_archivo_texto(archivo:str)->list:
+    '''
+    Carga un archivo de texto y devuelve una lista ocn las palabras del archivo
+    '''
+    with open(archivo,'r') as file:
+        oraciones=file.readlines()
+    return oraciones
+
+if __name__=='__main__':
+    lista=carga_archivo_texto('./plantillas/plantilla-0.txt')
+    for elemento in lista:
+        print(elemento)
