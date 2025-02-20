80f5ccb50170b71aaf65919549de701cc5112f12
sshoupm <sofiashoup@gmail.com>
2025-02-20 10:58:52 -0700

    Agregamos modificaciones a Sport.py para poder generar archivos en formato txt y json, y creamos la clase Team, que toma elementos de las clases Athlete y Sport

--- games/Sport.py
+++ games/Sport.py
@@ -24,13 +24,38 @@ class Sport:
         return {"name":self.name, "players":self.players, "league":self.league}
 
 if __name__== "__main__":
-    nfl=Sport("Football", 11, "NFL")
-    print(nfl)
-    print(repr(nfl))
-    print(nfl.to_json())
-    lmp=Sport("Baseball","9","LMP")
-    print(lmp)
-    print(repr(lmp))
-    print(lmp.to_json())
-    lmp2=eval(repr(lmp))
-    print(lmp2)
+    s=Sport("Soccer",11,"FIFA")
+    print(s)
+    print(repr(s))
+    print(s.to_json())
+    nfl=Sport("Football", "11", "NFL")
+    lmp=Sport("Baseball",9,"LMP")
+    mlb=Sport("Baseball",9,"MLB")
+    lmx=Sport("Soccer",11,"Liga MX")
+    nba=Sport("Basketball",5,"NBA")
+    lista_deportes=[nfl,lmp,mlb,lmx,nba,s]
+    archivo_deportes="deportes.txt"
+    with open(archivo_deportes, "w") as file:
+        for d in lista_deportes:
+            file.write(repr(d)+"\n")
+    sport_list=[]
+    with open(archivo_deportes, "r") as file:
+        for line in file:
+            d=eval(line)
+            sport_list.append(d)
+    print(sport_list)
+    print(sport_list[0].to_json())
+    #Escribiremos el archivo en formato JSON
+    import json
+    archivo_json="deportes.json"
+    #Convert all sports to JSON format
+    sports_json=[sport.to_json() for sport in sport_list]
+    #Write the entire list as a single JSON array
+    with open(archivo_json, "w") as file:
+        json.dump(sports_json, file, indent=4)
+
+    #Leemos el archivo JSON
+    sport_list_json=[]
+    with open(archivo_json, "r") as file:
+        sport_list_json=json.load(file)
+    print(sport_list_json)
