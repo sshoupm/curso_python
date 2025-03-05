6da936ff4e58dedd75a9aa5d36193c22c3972438
sshoupm <sofiashoup@gmail.com>
2025-03-05 10:42:33 -0700

    Hola mundo funcional!

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,11 +4,25 @@ app = Flask(__name__)
 
 @app.route("/") # home or root of website
 def index():
-    return '<html><head><title>HELLO WORLD</title></head><body><h1>Hello World</h1><p>Ir a<a href="/about">about</a></p></body></html>'
+    return '''<html>
+                <head>
+                    <title>HELLO WORLD</title>
+                </head>
+                <body><h1>Hello World</h1>
+                <p>Ir a la página de <a href="/about">Acerca de</a></p>
+                </body>
+            </html>'''
 
 @app.route("/about") # info about this site
 def about():
-    return '<html><head><title>About this page</title></head><body>Everythinf about this website. Back to<a href="/">Hello World</a></body></html>'
+    return '''<html>
+                <head>
+                <title>Acerca de</title>
+                </head>
+                <body><h1>Acerca de</h1>
+                <p>Ir a la página de <a href="/">Inicio</a><p>
+                </body>
+            </html>'''
 
 if __name__ == "__main__":
     app.run(debug=True)
