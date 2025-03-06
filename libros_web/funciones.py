55400f6a191fb0f969a403a24d3e875af0389384
sshoupm <sofiashoup@gmail.com>
2025-03-06 10:31:57 -0700

    Creamos la función busca_en_titulo(), y probamos que funciona correctamente.

--- libros_web/funciones.py
+++ libros_web/funciones.py
@@ -8,10 +8,23 @@ def lee_archivo_csv(archivo:str)->list:
     
 def crea_diccionario_titulos(lista:list)->dict:
      '''Crea un diccionario con los títulos de los libros como clave y el resto de los datos como valores'''
-     return {x['Titulo']:x for x in lista}
-    
+     return {x['title']:x for x in lista}
+
+def busca_en_titulo(diccionario,palabra)->list:
+    '''Busca palabra en título de la lista de diccionarios'''
+    lista=[]
+    palabra=palabra.lower()
+    for titulo, libro in diccionario.items():
+        if palabra in titulo.lower():
+            lista.append(libro)
+    return lista    
+
 if __name__ == "__main__":
     archivo_csv='booklist2000.csv'
+    #print(archivo_csv)
     lista_libros=lee_archivo_csv(archivo_csv)
     diccionario_libros=crea_diccionario_titulos(lista_libros)
-    (lista_libros)
+    #print(diccionario_libros)
+    print(lista_libros)
+    lista_resultados=busca_en_titulo(diccionario_libros,'rebels')
+    print(lista_resultados)
